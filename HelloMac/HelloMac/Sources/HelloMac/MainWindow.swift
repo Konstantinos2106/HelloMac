bba9f9c844017da51f73a3b96096822c4d7f6563
Konstantinos <268332020+Konstantinos2106@users.noreply.github.com>
2026-07-03 11:24:35 +0300

    HelloMac

--- HelloMac/HelloMac/Sources/HelloMac/MainWindow.swift
+++ HelloMac/HelloMac/Sources/HelloMac/MainWindow.swift
@@ -270,10 +270,10 @@ class MainWindowController: NSWindowController, NSWindowDelegate {
         centerWrapper.addSubview(deleteBtn)
 
         let keys: [(String, String)] = [
-            ("1",""), ("2","ABC"), ("3","DEF"),
-            ("4","GHI"), ("5","JKL"), ("6","MNO"),
-            ("7","PQRS"), ("8","TUV"), ("9","WXYZ"),
-            ("*",""), ("0","+"), ("#","")
+            ("1",""), ("2",""), ("3",""),
+            ("4",""), ("5",""), ("6",""),
+            ("7",""), ("8",""), ("9",""),
+            ("*",""), ("0",""), ("#","")
         ]
 
         let gridStack = NSStackView()
